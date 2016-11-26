b0f99d6ca9510f9f71e7b91811fb822c2ce05d46
Mark Andrews <mjandrews.net@gmail.com>
2016-11-26 12:38:45 +0000

    Fix setup

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(name = "gustav",
           'configobj>=5.0.6',
           'docopt>=0.6.2',
           'numpy>=1.11.2',
-          'scipy>=1.0',
           'mpi4py>=2.0.0',
           'sympy>=1.0'
       ]
